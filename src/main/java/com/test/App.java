5ad441e01af4ee1730c6ac4ac77dd1e2c87937ea
Keshabthecowboy <122006230+Keshabthecowboy@users.noreply.github.com>
2024-01-18 12:08:51 +0545

    Update App.java

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -2,7 +2,7 @@ package com.test;
 
 class App {
 	public static void main(String[] args){
-		Systemhello.out.println("This is Devops Class Running for teachers and students");
+		System.out.println("This is Devops Class Running for teachers and students");
 		
 	}
 
